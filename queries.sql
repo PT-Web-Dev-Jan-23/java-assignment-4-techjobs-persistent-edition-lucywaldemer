a4e05b41838b1f86cfe11422fb52b5e14445f2a7
Lu Waldemer <lucywaldemer@gmail.com>
2023-06-06 10:09:43 -0500

    still having errors

--- queries.sql
+++ queries.sql
@@ -1,7 +1,12 @@
 -- Part 1: Test it with SQL
 --id(integer), employer(string), name(string), skills(arrayList)
 -- Part 2: Test it with SQL
-
+-- getting a bean error, cannot bootRun yet
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
+SELECT id,
+    first_name,
+    last_name,
+  FROM customer
+  ORDER BY last_name ASC;
